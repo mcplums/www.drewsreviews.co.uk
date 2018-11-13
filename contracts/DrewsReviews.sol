11e793c5dd49d386a0ba43fa62b17129e8c78f36
Andrew Stanger <andrew@stanger.co.uk>
2018-11-13 19:17:24 +0000

    end of day commit, half way through adding userreviews, contract wont compile

--- contracts/DrewsReviews.sol
+++ contracts/DrewsReviews.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.4.23;
 contract DrewsReviews {
     
     uint public reviewIndex;
+    uint public userReviewIndex;
     
     mapping (uint => Review) reviewList;
+    mapping (uint =>  mapping(uint => Review)) userReviewList;
 
 	struct Review {
 		uint id;
@@ -13,11 +15,18 @@ contract DrewsReviews {
 		uint reviewDate;
 		uint score;
 		string imageSource;
+	}
 
+	struct userReview {
+		uint id;
+		string userName;
+		string review;
+		uint score;
 	}
 	
 constructor () public {
     reviewIndex = 0;
+    userReviewIndex = 0;
 }
 
 function addReview(string _name, string _review, uint _reviewdate, uint _score, string _imageSource) public {
@@ -25,6 +34,14 @@ function addReview(string _name, string _review, uint _reviewdate, uint _score, 
     
     Review memory review = Review (reviewIndex, _name, _review, _reviewdate, _score, _imageSource );
     reviewList[reviewIndex] = review;
+    
+}
+
+function addUserReview(uint _id, string _username, string _review, uint _score) public {
+    userReviewIndex += 1;
+    
+    userReview memory _userReview = userReview (userReviewIndex, _username, _review, _score);
+    userReviewList[_id][userReviewIndex] = _userReview;
     
 }
 
